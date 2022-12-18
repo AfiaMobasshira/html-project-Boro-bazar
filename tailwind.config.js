c88127d1db90b0a2080fdbad02538103615d9d00
AfiaMobasshira <109555094+AfiaMobasshira@users.noreply.github.com>
2022-12-19 04:47:28 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      
+    },
+  },
+  variants: {
+    extend: {
+      rotate: ['group-hover'],
+      scale: ['group-hover'],
+    },
+  },
+  plugins: [],
+}
